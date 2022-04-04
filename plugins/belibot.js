2339905f8df00a9d4cdc9aa446cb2f3cdd9537fe
AhmadBOTV3z <101960570+AhmadBOTV3z@users.noreply.github.com>
2022-04-04 17:45:42 +0800

    Update belibot.js

--- plugins/belibot.js
+++ plugins/belibot.js
@@ -1,18 +1,18 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
 	conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `╠═〘 Beli Bot 〙 ═
-╠➥ *1 Minggu* :      *Rp 5.000*
-╠➥ *1 Bulan* :      *Rp 10.000*
-╠➥ *Permanen* : *Rp 25.000*
-╠➥ *Trial* :   *Rp 0*
+╠➥ *VIA PULSA* :      *Rp 10.000*
+╠➥ *VIA DANA* :      *Rp 10.000*
+╠➥ *VIA GOPAY* : *Rp 10.000*
+╠➥ *PREMIUM* :   *Rp 5.000*
 ║
-╠═〘 𝐏𝐄𝐌𝐁𝐀𝐘𝐀𝐑𝐀𝐍 〙 ═
-╠➥ Topup/Pin
+╠═〘 METODE PEMBAYARAN 〙 ═
+╠➥ DANA/GOPAY/PULSA
 ║
 ╠═ Tertarik Untuk Beli Bot Ini?
 ╠➥Klil Button Di Bawah
 ║
-╠═〘 Hyzer 〙 ═`.trim(), '❖ Kanao-Bot', 'Gopay', '#viadigi', 'Dana', '#viaumobile', m)
+╠═〘 AHMADBOT 〙 ═`.trim(), '❖ AhmadBOT', 'Gopay', '#viagopay', 'Dana', '#viadana', m)
 }
 
 handler.command = /^sewabot$/i
